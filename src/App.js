4ff8818ef7d5a494602a54226f6befdecb875200
unknown <sojapril16@gmail.com>
2024-01-10 23:39:02 +0100

    new3

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
 
 
   return (
-    <div>
+    <div className=' font-body'>
       <Searchbar />
       <Home />
     </div>
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,23 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
+  darkMode: 'class',
   content: [
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
+    screens: {
+      'big': {'max': '1300px'},
+      // => @media (max-width: 1300px) { ... }
+      'md': {'max': '1030px'},
+      // => @media (max-width: 1030px) { ... }
+      'tab': {'max': '790px'},
+      // => @media (max-width: 1030px) { ... }
+      'mobile': {'max': '430px'},
+      // => @media (max-width: 430px) { ... }
+    },
+    fontFamily:{
+      body: ['Nunito Sans']
+    },
     extend: {},
   },
   plugins: [],
