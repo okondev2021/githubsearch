0e178a998a5738dd38310aa9a7b9dff9f6be5a55
unknown <sojapril16@gmail.com>
2024-01-11 02:39:16 +0100

    new7

--- src/components/home.jsx
+++ src/components/home.jsx
@@ -2,9 +2,9 @@ import { format } from "date-fns";
 const Home = ({fetchedResult}) => {
     
     return (
-        <div className="grid grid-cols-4 gap-10  bg-dark-main rounded-xl py-7 px-4 text-white">
-            <section className=" col-span-1 flex justify-center items-center">
-                <img className=" rounded-full aspect-square w-[100%] " src={fetchedResult && fetchedResult['avatar_url']} alt="profile pic" />
+        <div className="grid grid-cols-4 gap-5  bg-dark-main rounded-xl py-7 px-5 text-white">
+            <section className=" col-span-1 flex justify-center h-full">
+                <img className=" rounded-full aspect-square w-[80px] h-[80px] " src={fetchedResult && fetchedResult['avatar_url']} alt="profile pic" />
             </section>
             <section className=" col-span-3">
                 <header className="flex justify-between">
@@ -17,8 +17,25 @@ const Home = ({fetchedResult}) => {
                         <p className=" text-xs">Joined {fetchedResult && format(new Date(fetchedResult['created_at']), "d MMM yyyy") }</p>
                     </div>
                 </header>
-                <section className=" bg-dark-background">
-                    helllo
+                <section className=" bg-dark-background flex justify-between items-center px-4 py-4 rounded-xl mt-6">
+                    <div className="flex flex-col">
+                        <span className="text-center text-xs">Repos</span>
+                        <p className="text-center">{fetchedResult && fetchedResult['public_repos']}</p>
+                    </div>
+                    <div>
+                        <span className="text-center text-xs">Followers</span>
+                        <p className="text-center">{fetchedResult && fetchedResult['followers']}</p>
+                    </div>
+                    <div>
+                        <span className="text-center text-xs">Following</span>
+                        <p className="text-center">{fetchedResult && fetchedResult['following']}</p>
+                    </div>
+                </section>
+                <section className="grid grid-cols-2">
+                    <div></div>
+                    <div></div>
+                    <div></div>
+                    <div></div>
                 </section>
                 
             </section>
